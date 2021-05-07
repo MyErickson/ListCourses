19af2b0e52a6038f0c7a1f07e9a47bdb58253616
erickson <erickson@digitalcube.net>
2021-05-07 17:50:05 +0200

    add header

---
+++ App/Components/Header/Head.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { View, Text } from 'react-native'
+
+import {Header  } from 'react-native-elements'
+
+const Head = () => {
+
+
+    return (
+        <View style={{backgroundColor:'white'}}>
+            <Header
+                containerStyle={{bottom:-0.3}}
+                backgroundColor='#141415'
+                centerComponent={ <Text style={{color:"white"}}>Mes courses</Text>}
+            />
+        </View>
+    )
+}
+
+export default Head
